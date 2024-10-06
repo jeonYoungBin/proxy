162c02553fdb4f007077bbb100ebb1472f16bce2
전용빈 <cjswkdl@naver.com>
2024-10-06 20:35:41 +0900

    CGLIB - 테스트 코드 작성

---
+++ src/test/java/hello/proxy/hello/cglib/CglibTest.java
@@ -0,0 +1,22 @@
+package hello.proxy.hello.cglib;
+
+import hello.proxy.common.service.ConcreteService;
+import hello.proxy.hello.cglib.code.TimeMethodInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.cglib.proxy.Enhancer;
+
+@Slf4j
+public class CglibTest {
+
+    @Test
+    void cglib() {
+        ConcreteService target = new ConcreteService();
+
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(ConcreteService.class);
+        enhancer.setCallback(new TimeMethodInterceptor(target));
+        ConcreteService proxy = (ConcreteService) enhancer.create();
+        proxy.call();
+    }
+}
